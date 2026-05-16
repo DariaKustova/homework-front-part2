8c3b3795ff4940e32e8d4cc7b5da554662dd7ffb
Daria <freedomonlyme777@gmail.com>
2026-05-16 14:37:11 +0300

    update

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | {type: "sort"; payload: "up" | "down"}
+  | {type: "check"; payload: number};
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      const sortResult = [...state].sort((a, b) =>
+        a.name.localeCompare(b.name),
+      );
+      // by name
+      return action.payload === "down" ? sortResult.reverse() : sortResult; // need to fix
     }
-}
+    case "check": {
+      return state.filter((u) => u.age >= 18);
+    }
+    default:
+      return state;
+  }
+};
