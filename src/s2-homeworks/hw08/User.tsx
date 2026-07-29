9a5dfad5c3393e11173cd3f28e7daeca8d2ec5b2
Daria <freedomonlyme777@gmail.com>
2026-07-29 08:48:59 +0300

    styles

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import { UserType } from './HW8'
-import s from './HW8.module.css'
+import React from "react";
+import {UserType} from "./HW8";
+import s from "./HW8.module.css";
 
 // types
 type UserPropsType = {
-    u: UserType
-}
+  u: UserType;
+};
 
-const User: React.FC<UserPropsType> = ({ u }) => {
-    return (
-        <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
-            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
-                {u.name}
-                {/*отобразить имя*/}
+const User: React.FC<UserPropsType> = ({u}) => {
+  return (
+    <tr id={"hw8-user-" + u._id + "-" + u.age} className={s.item}>
+      <td id={"hw8-user-name-" + u._id} className={s.nameCol}>
+        {u.name}
+        {/*отобразить имя*/}
+      </td>
+      <td id={"hw8-user-age-" + u._id} className={s.ageCol}>
+        {u.age}
 
-            </td>
-            <td id={'hw8-user-age-' + u._id}>
-                {u.age}
-                {/*отобразить возраст*/}
+        {/*отобразить возраст*/}
+      </td>
+    </tr>
+  );
+};
 
-            </td>
-        </tr>
-    )
-}
-
-export default User
+export default User;
